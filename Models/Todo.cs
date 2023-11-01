b6a2cb15d9ed167c74c58485e7f5abcc6daa05d4
ze-fernando <jfenginner935@gmail.com>
2023-10-31 22:45:53 -0300

    feat: bugs and more bugs

--- Models/Todo.cs
+++ Models/Todo.cs
@@ -4,13 +4,17 @@ using System.ComponentModel.DataAnnotations;
 public class Todo
 {
     public int id { get; set; }
+
     [DisplayName("Titulo")]
     [Required(ErrorMessage = "Titulo deve ser obrigatório")]
     public string Title { get; set; }
+
     [DisplayName("Concluida")]
     public bool Done { get; set; }
+
     [DisplayName("Atualizada em")]
     public DateTime Updated { get; set; } = DateTime.Now;
+    
     [DisplayName("Usuario")]
     public string User { get; set; }
     
