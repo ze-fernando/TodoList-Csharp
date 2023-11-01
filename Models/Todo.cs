c543091977e89e827d45b5a673574c8c2d21dee4
ze-fernando <jfenginner935@gmail.com>
2023-10-31 21:50:17 -0300

    feat: create TODO

---
+++ Models/Todo.cs
@@ -0,0 +1,18 @@
+using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+public class Todo
+{
+    public int id { get; set; }
+    [DisplayName("Titulo")]
+    [Required(ErrorMessage = "Titulo deve ser obrigatório")]
+    public string Title { get; set; }
+    [DisplayName("Concluida")]
+    public bool Done { get; set; }
+    [DisplayName("Atualizada em")]
+    public DateTime Updated { get; set; } = DateTime.Now;
+    [DisplayName("Usuario")]
+    public string User { get; set; }
+    
+}
